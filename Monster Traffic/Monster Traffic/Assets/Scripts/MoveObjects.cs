975050ecbd939aad1a84f4f8937a66ab6e70528a
Nikita Shalimo <MegaFrostBall@gmail.com>
2019-10-25 18:29:11 +0300

    Add Monster Traffic

---
+++ Monster Traffic/Monster Traffic/Assets/Scripts/MoveObjects.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjects : MonoBehaviour
+{
+
+    public float speed = 2.3f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (transform.position.y < -6)
+            Destroy(gameObject);
+        
+    }
+}
