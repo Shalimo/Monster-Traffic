36da8a4ef1f436f86bb304cbd48586a8656d83d8
Nikita Shalimo <MegaFrostBall@gmail.com>
2019-10-28 00:58:01 +0300

    Spawn cars added

---
+++ Monster Traffic/Monster Traffic/Assets/Scripts/SpawnCars.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnCars : MonoBehaviour
+
+    
+{
+    public GameObject[] cars;
+    private float[] positions = { -1.86f, -0.65f, 0.59f, 1.76f };
+
+    void Start()
+    {
+        StartCoroutine(spawn());
+    }
+
+    IEnumerator spawn()
+    {
+        while(true)
+        {
+            Instantiate(
+                cars[Random.Range(0, cars.Length)],
+                new Vector3(positions[Random.Range(0, 4)], 6f, 0),
+                Quaternion.Euler(new Vector3(90, 180, 0))
+                );
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+    
+}
