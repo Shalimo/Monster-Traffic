19b90a18c20018d143f92ebc65d3a09acc60d37d
Nikita Shalimo <MegaFrostBall@gmail.com>
2019-10-27 22:46:53 +0300

    Endless road added

---
+++ Monster Traffic/Monster Traffic/Assets/Scripts/MoveTrack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrack : MonoBehaviour
+{
+
+    public float speed;
+
+    private Vector2 offset;
+
+    private void Update()
+    {
+        offset = new Vector2(0, Time.time * speed); //params OsX, OsY
+
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
