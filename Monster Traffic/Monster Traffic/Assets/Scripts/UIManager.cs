d7eb3b79fdc03e4cdabf400f4e86ce6d70b1b692
Nikita Shalimo <MegaFrostBall@gmail.com>
2019-11-10 23:25:24 +0300

    Add UIManager

---
+++ Monster Traffic/Monster Traffic/Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Text scoreText;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        InvokeRepeating("scoreUpdate", 1.0f, 0.5f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+        
+        
+        
+    }
+
+    void scoreUpdate()
+    {
+        score++;
+    }
+}
