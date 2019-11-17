8c793120294c47d29381f4e44446b7ae7333496f
Nikita Shalimo <MegaFrostBall@gmail.com>
2019-11-17 17:30:58 +0300

    Add MainMenu

---
+++ Monster Traffic/Monster Traffic/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+
+}
